a839b3910e3244563a4caec1c3d615977f961bbf
Nick-Ol <clement.nicolle@student.ecp.fr>
2015-02-24 14:36:56 +0100

    OnlyOneChoiceInColumnVisitor modified

--- SudokuSolver/OnlyOneChoiceInColumnVisitor.cpp
+++ SudokuSolver/OnlyOneChoiceInColumnVisitor.cpp
@@ -14,41 +14,58 @@ OnlyOneChoiceInColumnVisitor::~OnlyOneChoiceInColumnVisitor()
 bool OnlyOneChoiceInColumnVisitor::Visit(Grid& ioGrid) const
 {
 	bool success = false;
-
-	LastCellFinder lcf1(RegionHolder(ioGrid.Get_rNW()).LeftColumn(),
-		RegionHolder(ioGrid.Get_rW()).LeftColumn(), RegionHolder(ioGrid.Get_rSW()).LeftColumn());
+	ColumnHolder ch1_1 = RegionHolder(ioGrid.Get_rNW()).LeftColumn();
+	ColumnHolder ch1_2 = RegionHolder(ioGrid.Get_rW()).LeftColumn();
+	ColumnHolder ch1_3 = RegionHolder(ioGrid.Get_rSW()).LeftColumn();
+	LastCellFinder lcf1(ch1_1, ch1_2, ch1_3);
 	success |= lcf1.fill();
 
-	LastCellFinder lcf2(RegionHolder(ioGrid.Get_rNW()).MiddleColumn(),
-		RegionHolder(ioGrid.Get_rW()).MiddleColumn(), RegionHolder(ioGrid.Get_rSW()).MiddleColumn());
+	ColumnHolder ch2_1 = RegionHolder(ioGrid.Get_rNW()).MiddleColumn();
+	ColumnHolder ch2_2 = RegionHolder(ioGrid.Get_rW()).MiddleColumn();
+	ColumnHolder ch2_3 = RegionHolder(ioGrid.Get_rSW()).MiddleColumn();
+	LastCellFinder lcf2(ch2_1, ch2_2, ch2_3);
 	success |= lcf2.fill();
 
-	LastCellFinder lcf3(RegionHolder(ioGrid.Get_rNW()).RightColumn(),
-		RegionHolder(ioGrid.Get_rW()).RightColumn(), RegionHolder(ioGrid.Get_rSW()).RightColumn());
+	ColumnHolder ch3_1 = RegionHolder(ioGrid.Get_rNW()).RightColumn();
+	ColumnHolder ch3_2 = RegionHolder(ioGrid.Get_rW()).RightColumn();
+	ColumnHolder ch3_3 = RegionHolder(ioGrid.Get_rSW()).RightColumn();
+	LastCellFinder lcf3(ch3_1, ch3_2, ch3_3);
 	success |= lcf3.fill();
 
-	LastCellFinder lcf4(RegionHolder(ioGrid.Get_rN()).LeftColumn(),
-		RegionHolder(ioGrid.Get_rC()).LeftColumn(), RegionHolder(ioGrid.Get_rS()).LeftColumn());
+	ColumnHolder ch4_1 = RegionHolder(ioGrid.Get_rN()).LeftColumn();
+	ColumnHolder ch4_2 = RegionHolder(ioGrid.Get_rC()).LeftColumn();
+	ColumnHolder ch4_3 = RegionHolder(ioGrid.Get_rS()).LeftColumn();
+	LastCellFinder lcf4(ch4_1, ch4_2, ch4_3);
 	success |= lcf4.fill();
 
-	LastCellFinder lcf5(RegionHolder(ioGrid.Get_rN()).MiddleColumn(),
-		RegionHolder(ioGrid.Get_rC()).MiddleColumn(), RegionHolder(ioGrid.Get_rS()).MiddleColumn());
+	ColumnHolder ch5_1 = RegionHolder(ioGrid.Get_rN()).MiddleColumn();
+	ColumnHolder ch5_2 = RegionHolder(ioGrid.Get_rC()).MiddleColumn();
+	ColumnHolder ch5_3 = RegionHolder(ioGrid.Get_rS()).MiddleColumn();
+	LastCellFinder lcf5(ch5_1, ch5_2, ch5_3);
 	success |= lcf5.fill();
 
-	LastCellFinder lcf6(RegionHolder(ioGrid.Get_rN()).RightColumn(),
-		RegionHolder(ioGrid.Get_rC()).RightColumn(), RegionHolder(ioGrid.Get_rS()).RightColumn());
+	ColumnHolder ch6_1 = RegionHolder(ioGrid.Get_rN()).RightColumn();
+	ColumnHolder ch6_2 = RegionHolder(ioGrid.Get_rC()).RightColumn();
+	ColumnHolder ch6_3 = RegionHolder(ioGrid.Get_rS()).RightColumn();
+	LastCellFinder lcf6(ch6_1, ch6_2, ch6_3);
 	success |= lcf6.fill();
 
-	LastCellFinder lcf7(RegionHolder(ioGrid.Get_rNE()).LeftColumn(),
-		RegionHolder(ioGrid.Get_rE()).LeftColumn(), RegionHolder(ioGrid.Get_rSE()).LeftColumn());
+	ColumnHolder ch7_1 = RegionHolder(ioGrid.Get_rNE()).LeftColumn();
+	ColumnHolder ch7_2 = RegionHolder(ioGrid.Get_rE()).LeftColumn();
+	ColumnHolder ch7_3 = RegionHolder(ioGrid.Get_rSE()).LeftColumn();
+	LastCellFinder lcf7(ch7_1, ch7_2, ch7_3);
 	success |= lcf7.fill();
 
-	LastCellFinder lcf8(RegionHolder(ioGrid.Get_rNE()).MiddleColumn(),
-		RegionHolder(ioGrid.Get_rE()).MiddleColumn(), RegionHolder(ioGrid.Get_rSE()).MiddleColumn());
+	ColumnHolder ch8_1 = RegionHolder(ioGrid.Get_rNE()).MiddleColumn();
+	ColumnHolder ch8_2 = RegionHolder(ioGrid.Get_rE()).MiddleColumn();
+	ColumnHolder ch8_3 = RegionHolder(ioGrid.Get_rSE()).MiddleColumn();
+	LastCellFinder lcf8(ch8_1, ch8_2, ch8_3);
 	success |= lcf8.fill();
 
-	LastCellFinder lcf9(RegionHolder(ioGrid.Get_rNE()).RightColumn(),
-		RegionHolder(ioGrid.Get_rE()).RightColumn(), RegionHolder(ioGrid.Get_rSE()).RightColumn());
+	ColumnHolder ch9_1 = RegionHolder(ioGrid.Get_rNE()).RightColumn();
+	ColumnHolder ch9_2 = RegionHolder(ioGrid.Get_rE()).RightColumn();
+	ColumnHolder ch9_3 = RegionHolder(ioGrid.Get_rSE()).RightColumn();
+	LastCellFinder lcf9(ch9_1, ch9_2, ch9_3);
 	success |= lcf9.fill();
 
 	return success;
