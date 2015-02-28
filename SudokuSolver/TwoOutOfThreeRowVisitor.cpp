972a9765f30932d97f6383b3fdbb64ca5e0b52eb
Mathurin Massias <mathurin.massias@gmail.com>
2015-02-28 17:42:11 +0100

    Corrections to 2outOf3Row

--- SudokuSolver/TwoOutOfThreeRowVisitor.cpp
+++ SudokuSolver/TwoOutOfThreeRowVisitor.cpp
@@ -1,7 +1,9 @@
 #include "TwoOutOfThreeRowVisitor.h"
 #include "NineHolder.h"
 #include "ValueEliminator.h"
-//#include "RegionHolder.h"
+#include <set>
+
+using namespace std;
 
 TwoOutOfThreeRowVisitor::TwoOutOfThreeRowVisitor()
 {
@@ -25,79 +27,85 @@ bool TwoOutOfThreeRowVisitor::Visit(Grid& ioGrid) const
 		{
 			unsigned char rowDigitAbsent = 0;
 			unsigned char absences = 0;
-			set<unsigned char> ColumnAbsence;
-			ColumnAbsence.insert(0);
-			ColumnAbsence.insert(1);
-			ColumnAbsence.insert(2);
+			set<unsigned char> columnAbsence;
+			columnAbsence.insert(0);
+			columnAbsence.insert(1);
+			columnAbsence.insert(2);
 			if (!FullTopLine.isValuePresent(digit))
 			{ 
-				rowDigitAbsent = 3 * i; absences++; 
+				rowDigitAbsent = 3 * i; 
+				absences++; 
 			}
-			else{ //add Column in which the digit in present to the set ColumnAbsence
+			else{ //delete Column in which the digit in present from the set columnAbsence
 				if (FullTopLine.GetCell(0).GetValue() == digit || FullTopLine.GetCell(1).GetValue() == digit
 					|| FullTopLine.GetCell(2).GetValue() == digit)
 				{
-					ColumnAbsence.erase(0);
+					columnAbsence.erase(0);
 				}
 				if (FullTopLine.GetCell(3).GetValue() == digit || FullTopLine.GetCell(4).GetValue() == digit
 					|| FullTopLine.GetCell(5).GetValue() == digit)
 				{
-					ColumnAbsence.erase(1);
+					columnAbsence.erase(1);
 				}
 				if (FullTopLine.GetCell(6).GetValue() == digit || FullTopLine.GetCell(7).GetValue() == digit
 					|| FullTopLine.GetCell(8).GetValue() == digit)
 				{
-					ColumnAbsence.erase(2);
+					columnAbsence.erase(2);
 				}
 				
 			}
 			if (!FullMiddleLine.isValuePresent(digit))
 			{ 
-				rowDigitAbsent = 3 * i + 1; absences++;
+				rowDigitAbsent = 3 * i + 1; 
+				absences++;
 			}
-			else{ //add Column in which the digit in present to the set ColumnAbsence
+			else{ //add Column in which the digit in present to the set columnAbsence
 				if (FullMiddleLine.GetCell(0).GetValue() == digit || FullMiddleLine.GetCell(1).GetValue() == digit
 					|| FullMiddleLine.GetCell(2).GetValue() == digit)
 				{
-					ColumnAbsence.erase(0);
+					columnAbsence.erase(0);
 				}
 				if (FullMiddleLine.GetCell(3).GetValue() == digit || FullMiddleLine.GetCell(4).GetValue() == digit
 					|| FullMiddleLine.GetCell(5).GetValue() == digit)
 				{
-					ColumnAbsence.erase(1);
+					columnAbsence.erase(1);
 				}
 				if (FullMiddleLine.GetCell(6).GetValue() == digit || FullMiddleLine.GetCell(7).GetValue() == digit
 					|| FullMiddleLine.GetCell(8).GetValue() == digit)
 				{
-					ColumnAbsence.erase(2);
+					columnAbsence.erase(2);
 				}
 			}
-			if (!FullBottomLine.isValuePresent(digit)){ rowDigitAbsent = 3 * i + 2; absences++; }
-			else{ //add Column in which the digit in present to the set ColumnAbsence
+			if (!FullBottomLine.isValuePresent(digit))
+			{ 
+				rowDigitAbsent = 3 * i + 2; 
+				absences++; 
+			}
+			else{ //erase Column in which the digit in present from the set columnAbsence
 				if (FullBottomLine.GetCell(0).GetValue() == digit || FullBottomLine.GetCell(1).GetValue() == digit
 					|| FullBottomLine.GetCell(2).GetValue() == digit)
 				{
-					ColumnAbsence.erase(0);
+					columnAbsence.erase(0);
 				}
 				if (FullBottomLine.GetCell(3).GetValue() == digit || FullBottomLine.GetCell(4).GetValue() == digit
 					|| FullBottomLine.GetCell(5).GetValue() == digit)
 				{
-					ColumnAbsence.erase(1);
+					columnAbsence.erase(1);
 				}
 				if (FullBottomLine.GetCell(6).GetValue() == digit || FullBottomLine.GetCell(7).GetValue() == digit
 					|| FullBottomLine.GetCell(8).GetValue() == digit)
 				{
-					ColumnAbsence.erase(2);
+					columnAbsence.erase(2);
 				}
 			}
 
-			if (absences == 1) //we've been twice in the else, column absence has one element exactly
+			if (absences == 1) //we've been twice in the else, rowAbsence has one element exactly
 			{
-				//the digit is Absent of exactly one row out of three
+				//the digit is absent of exactly one row out of three
 				//it is absent of row rowDigitAbsent
-				//there is exactly 1 element in the set ColumnAbsence: 0, 1 or 2
+				//there is exactly 1 element in the set columnAbsence: 0, 1 or 2
 				//Get 3 nineholder (column) on the 3 corresponding columns (3colRegIndex, .., 3colRegIndex+2)
-				set<unsigned char>::iterator iter = ColumnAbsence.begin();
+				set<unsigned char>::iterator iter = columnAbsence.begin();
 				int colRegIndex = *iter;
 				//TODO: Loop
 				NineHolder nh1 = ioGrid.GetColumn(3 * colRegIndex);
@@ -108,14 +116,13 @@ bool TwoOutOfThreeRowVisitor::Visit(Grid& ioGrid) const
 				possiblePlaces.insert(0);
 				possiblePlaces.insert(1);
 				possiblePlaces.insert(2);
-
 				RegionHolder regHolder = ioGrid.GetRegion(rowDigitAbsent / 3, colRegIndex);
+
 				if (!regHolder.GetCell(rowDigitAbsent % 3, 0).IsEmpty()
 					|| nh1.isValuePresent(digit) )
 				{
 					possiblePlaces.erase(0);
 				}
-				//See if digit is present in 2 of them (when present, remove their index from possiblePlaces)
 				if (!regHolder.GetCell(rowDigitAbsent % 3, 1).IsEmpty()
 					|| nh2.isValuePresent(digit))
 				{
